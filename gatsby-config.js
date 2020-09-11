d14a079e5a6006295e40de94af1f3be59ce96beb
gustaveH <ghabiman@gmail.com>
2020-09-11 16:49:58 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Gustave Portfolio",
+    description: "My Portfolio",
+    author: "@Gustave",
+    twitterUsername: "@_Habimana_",
+    image: "/twitter-img.png",
+    siteUrl: "https://www.gustavehabimana.com",
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   singleType : `about`
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [`jobs`, `projects`],
+        singleTypes: [`about`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `700`],
+          },
+          {
+            family: `Open Sans`,
+          },
+        ],
+      },
+    },
+  ],
+}
---
+++ src/components/Jobs.js
@@ -0,0 +1,72 @@
+import React from "react"
+import Title from "./Title"
+import { FaAngleDoubleRight, FaAlignRight } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: ASC }) {
+      nodes {
+        position
+        company
+        date
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+const Jobs = () => {
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { postion, company, date, desc } = jobs[value]
+  console.log(postion, company, date, desc)
+
+  return (
+    <section className="section jobs">
+      j
+      <Title title="experience" />
+      <div className="jobs-center">
+        {/* btn conatiner */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                ket={item.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{postion}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+        <Link to="/about" className="btn center-btn">
+          more info
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default Jobs
